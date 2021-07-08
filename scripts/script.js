8959c8c9d2e29e820df1656dddacf9e226b5952b
Alvigiar <officialgmvladmedvedkov@gmail.com>
2021-07-08 13:33:20 +0300

    day 3

--- scripts/script.js
+++ scripts/script.js
@@ -151,7 +151,7 @@ try { // если код внутри try вызывает ошибку, то п
     // });
   }
 
-  
+
   window.addEventListener('hashchange', () => { // при изменении хеша будут происходить действия
     hash = location.hash.substring(1)
     getGoods(renderGoodsList, 'category', hash)
@@ -180,20 +180,59 @@ try {
     throw 'This is not card good page'
   }
 
+  // получение необходимых данных в переменные
   const cardGoodImage = document.querySelector('.card-good__image')
   const cardGoodBrand = document.querySelector('.card-good__brand')
   const cardGoodTitle = document.querySelector('.card-good__title')
   const cardGoodPrice = document.querySelector('.card-good__price')
   const cardGoodColor = document.querySelector('.card-good__color')
+  const cardGoodSelectWrapper = document.querySelectorAll('.card-good__select__wrapper')
   const cardGoodColorList = document.querySelector('.card-good__color-list')
   const cardGoodSizes = document.querySelector('.card-good__sizes')
   const cardGoodSizesList = document.querySelector('.card-good__sizes-list')
   const cardGoodBuy = document.querySelector('.card-good__buy')
 
-  const renderCardGood = ({brand, name, cost, color, sizes, photo}) => {
-    console.log(brand)
+  generateList = data => data.reduce((html, item, i) => html + // перебор данных reduce
+    `<li class="card-good__select-item" data-id="${i}">${item}</li>`, '')
+
+  const renderCardGood = ([{ brand, name, cost, color, sizes, photo }]) => { // деструкция массива
+    cardGoodImage.src = `goods-image/${photo}`
+    cardGoodImage.alt = `${brand} ${name}`
+    cardGoodBrand.textContent = brand
+    cardGoodTitle.textContent = name
+    cardGoodPrice.textContent = `${cost} ₽`
+    if (color) { // вывод цвета
+      cardGoodColor.textContent = color[0]
+      cardGoodColor.dataset.id = 0
+      cardGoodColorList.innerHTML = generateList(color)
+    } else { // скрытие
+      cardGoodColor.style.display = 'none'
+    }
+    if (sizes) {
+      cardGoodSizes.textContent = sizes[0]
+      cardGoodSizes.dataset.id = 0
+      cardGoodSizesList.innerHTML = generateList(sizes)
+    } else {
+      cardGoodSizes.style.display = 'none'
+    }
   }
-  
+
+  cardGoodSelectWrapper.forEach(item => {
+    item.addEventListener('click', (e) => {
+      const target = e.target
+      if (target.closest('.card-good__select')) {
+        target.classList.toggle('card-good__select__open')
+      }
+
+      if (target.closest('.card-good__select-item')) {
+        const cardGoodSelect = item.querySelector('.card-good__select')
+        cardGoodSelect.textContent = target.textContent
+        cardGoodSelect.dataset.id = target.dataset.id
+        cardGoodSelect.classList.remove('card-good__select__open')
+      }
+    })
+  })
+
   getGoods(renderCardGood, 'id', hash)
 
 } catch (err) {
